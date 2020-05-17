894d0be15cdad9be50e23f5e8a0495e4496be4f5
fouadmagdy <fouad_magdy@hotmail.com>
2020-05-17 20:09:44 +0200

    first commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+
+        const { item, handleChange, handleSubmit, editItem } = this.props
+
+        return (
+            <div className="card card-body my-3">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                <div className="fas fa-book"></div>
+                            </div>
+                        </div>
+                        <input type="text" className="form-control text-capitialize" placeholder="Add Todo item" onChange={handleChange} value={item} />
+                    </div>
+                    <button type="submit" className="btn btn-block btn-primary mt-3 text-uppercase">Add item</button>
+                </form>
+            </div>
+        )
+    }
+}
---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import TodoItem from './TodoItem'
+
+export default class TodoList extends Component {
+    render() {
+
+        const { items, clearList, handleDelete, handleEdit } = this.props
+
+        return (
+            <section>
+                <ul className="listgroup my-5">
+                    <h3 className="text-capitialize text-center">todo list</h3>
+                    {items.map(item => {
+                        return (<TodoItem key={item.id} title={item.title} handleDelete={() => handleDelete(item.id)}
+                            handleEdit={() => handleEdit(item.id)}></TodoItem>)
+                    })}
+                </ul>
+                <button type="button" className="btn btn-danger btn-block text-uppercase mt-5" onClick={clearList}>clear list</button>
+
+            </section>
+        )
+    }
+}
